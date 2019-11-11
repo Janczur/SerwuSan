f7d6c566924008b44b9f26f566d610cf3cae409d
dzbanek <j.przybysz@grupatense.pl>
2019-11-11 11:45:40 +0100

    Little frontend changes

--- resources/views/billings/show.blade.php
+++ resources/views/billings/show.blade.php
@@ -4,13 +4,23 @@
 
         <div class="row">
 
-            <div class="col-lg-7 col-md-12">
+            <div class="col-lg-6 col-md-12">
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Dane</h6>
+                        <h4 class="m-0 font-weight-bold text-primary">Dane <small class="text-muted">(Tylko 10 pierwszych wyników)</small></h4>
                     </div>
                     <div class="card-body">
-                        Dane
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">Początek rozmowy</th>
+                                <th scope="col">Czas trwania <small>(sekundy)</small></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @each('billings.tbody._show', $billingData, 'billingData')
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -19,14 +29,35 @@
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
                         <div class="d-sm-flex align-items-center justify-content-between">
-                            <h6 class="m-0 font-weight-bold text-primary">Informacje o billingu</h6>
+                            <h4 class="m-0 font-weight-bold text-primary">{{ $billing->name }}</h4>
                             <a href="{{ route('billings.edit', $billing) }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-edit fa-sm text-white-50"></i> Edytuj billing</a>
                         </div>
                     </div>
                     <div class="card-body">
-                        <h1>{{ $billing->name }}</h1>
-                        <p>Opłata za dni robocze: {{ $billing->working_days_rate }}</p>
-                        <p>Opłata za sobotę: {{ $billing->saturday_rate }}</p>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="working_days_rate">Opłata za dni robocze:</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">zł</span>
+                                        </div>
+                                        <input class="form-control" id="working_days_rate" value="{{ $billing->working_days_rate }}" readonly>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="saturday_rate">Opłata w sobotę:</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">zł</span>
+                                        </div>
+                                        <input class="form-control" id="saturday_rate" value="{{ $billing->saturday_rate }}" readonly>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
